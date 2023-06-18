907c41bdc0213aa1b9e469f849f708416aec6781
Leastrio <jacob@leastr.io>
2023-06-18 00:30:22 -0600

    disable the guild delete event

--- lib/anti_ghost_ping/consumer.ex
+++ lib/anti_ghost_ping/consumer.ex
@@ -50,9 +50,9 @@ defmodule AntiGhostPing.Consumer do
     end
   end
 
-  def handle_event({:GUILD_DELETE, {%Nostrum.Struct.Guild{id: id}, false}, _ws_state}) do
-    AntiGhostPing.Repo.delete(id)
-  end
+  # def handle_event({:GUILD_DELETE, {%Nostrum.Struct.Guild{id: id}, false}, _ws_state}) do
+  #   AntiGhostPing.Repo.delete(id)
+  # end
 
   def handle_event({:INTERACTION_CREATE, %Nostrum.Struct.Interaction{type: type} = interaction, _ws_state}) do
     case type do

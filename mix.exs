0f6f3beeda8498c7ca04706baba9ada38835382e
Leastrio <jacob@leastr.io>
2024-05-14 19:03:51 -0600

    possibly add profiler

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule AntiGhostPing.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:mnesia, :logger, :runtime_tools],
+      extra_applications: [:mnesia, :logger, :runtime_tools, :fprof],
       mod: {AntiGhostPing.Application, []}
     ]
   end
